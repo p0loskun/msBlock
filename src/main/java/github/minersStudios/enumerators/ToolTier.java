324347ca46d341894d57b93d858eeb3b1798af2b
Poloskun_ <nikongricisin339@gmail.com>
2022-01-03 20:01:18 +0300

    edited complexity method

--- src/main/java/github/minersStudios/enumerators/ToolTier.java
+++ src/main/java/github/minersStudios/enumerators/ToolTier.java
@@ -15,54 +15,26 @@ public enum ToolTier
     public final int order;
     public final float speed;
 
-    ToolTier(int order, float speed)
-    {
+    ToolTier(int order, float speed) {
         this.order = order;
         this.speed = speed;
     }
 
-    public static ToolTier fromItemStack(ItemStack stack)
-    {
-        switch(stack.getType())
-        {
-            case WOODEN_SWORD:
-            case WOODEN_PICKAXE:
-            case WOODEN_AXE:
-            case WOODEN_SHOVEL:
-            case WOODEN_HOE:
-                return WOOD;
-            case STONE_SWORD:
-            case STONE_PICKAXE:
-            case STONE_AXE:
-            case STONE_SHOVEL:
-            case STONE_HOE:
-                return STONE;
-            case IRON_SWORD:
-            case IRON_PICKAXE:
-            case IRON_AXE:
-            case IRON_SHOVEL:
-            case IRON_HOE:
-                return IRON;
-            case GOLDEN_SWORD:
-            case GOLDEN_PICKAXE:
-            case GOLDEN_AXE:
-            case GOLDEN_SHOVEL:
-            case GOLDEN_HOE:
-                return GOLD;
-            case DIAMOND_SWORD:
-            case DIAMOND_PICKAXE:
-            case DIAMOND_AXE:
-            case DIAMOND_SHOVEL:
-            case DIAMOND_HOE:
-                return DIAMOND;
-            case NETHERITE_SWORD:
-            case NETHERITE_PICKAXE:
-            case NETHERITE_AXE:
-            case NETHERITE_SHOVEL:
-            case NETHERITE_HOE:
-                return NETHERITE;
-            default:
-                return ANY;
+    public static ToolTier fromItemStack(ItemStack stack) {
+        if (stack.getType().toString().contains("WOODEN_")) {
+            return WOOD;
+        } else if (stack.getType().toString().contains("STONE_")) {
+            return STONE;
+        } else if (stack.getType().toString().contains("IRON_")) {
+            return IRON;
+        } else if (stack.getType().toString().contains("GOLDEN_")) {
+            return GOLD;
+        } else if (stack.getType().toString().contains("DIAMOND_")) {
+            return DIAMOND;
+        } else if (stack.getType().toString().contains("NETHERITE_")) {
+            return NETHERITE;
         }
+        return ANY;
     }
+
 }
